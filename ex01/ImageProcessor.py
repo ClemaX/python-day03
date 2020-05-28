e15e104f80d74c034889f4ece2f4f72e86fe1924
Clément Hamada <clementhamada@protonmail.com>
2020-05-28 22:17:44 +0200

    ex01

---
+++ ex01/ImageProcessor.py
@@ -0,0 +1,21 @@
+from matplotlib import pyplot as plt, image as mpimg
+
+
+class ImageProcessor:
+    @staticmethod
+    def load(path):
+        """Load a PNG image into a numpy array"""
+        return plt.imread(path)
+
+    @staticmethod
+    def display(array):
+        """Display an image from a numpy array"""
+        plt.imshow(array)
+        plt.show()
+
+
+if __name__ == "__main__":
+    img = ImageProcessor.load('./test.png')
+    ImageProcessor.display(img)
+    while True:
+        pass
